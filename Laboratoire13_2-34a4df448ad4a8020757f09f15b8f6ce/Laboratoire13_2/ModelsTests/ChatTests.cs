2d52e53342317a17da7ac8d292f3389f22888a2e
2568832 <2568832@cegepmontpetit.ca>
2026-05-11 14:15:53 -0400

    Ex1-2 fait

---
+++ Laboratoire13_2-34a4df448ad4a8020757f09f15b8f6ce/Laboratoire13_2/ModelsTests/ChatTests.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Tests
+{
+    [TestClass()]
+    public class ChatTests
+    {
+        
+        Chat crééChatParDeffault ()
+        {
+            Chat chat = new Chat("A123", "Minou", 3);
+            return chat;
+        }
+
+        [TestMethod()]
+        public void Chat_Matricule_Null_Test()
+        {
+            
+
+            Assert.ThrowsException<ArgumentNullException>( 
+                () =>
+                {
+                    Chat chat = new Chat( null, "Minou", 3); 
+
+                }
+            );
+
+        }
+
+        [TestMethod()]
+        public void Chat_Matricule_4Caracteres_Test()
+        {
+
+
+            Assert.ThrowsException<ArgumentException>(
+                () =>
+                {
+                    Chat chat = new Chat("abc", "Minou", 3);
+
+                }
+            );
+
+        }
+
+        [TestMethod()]
+        public void Chat_Nom_null_Test()
+        {
+
+
+            Assert.ThrowsException<ArgumentNullException>(
+                () =>
+                {
+                    Chat chat = new Chat("abcd", "  ", 3);
+
+                }
+            );
+
+        }
+        [TestMethod()]
+        public void Chat_Nom_Moin_3Caracteres_Test()
+        {
+
+
+            Assert.ThrowsException<ArgumentException>(
+                () =>
+                {
+                    Chat chat = new Chat("abcd", "al", 3);
+
+                }
+            );
+
+        }
+
+        [TestMethod()]
+        public void Chat_Age_egale_zero_Test()
+        {
+
+
+            Assert.ThrowsException<ArgumentException>(
+                () =>
+                {
+                    Chat chat = new Chat("abcd", "allle", 0);
+
+                }
+            );
+
+        }
+
+
+        [TestMethod()]
+        public void Chat_Age_Plus_Grand_zero_Test()
+        {
+
+            Chat chat = new Chat("abcd", "allle", 12);
+
+            
+            Assert.AreNotEqual( chat.Age, 0 );
+            
+
+
+        }
+    }
+}
---
+++ Laboratoire13_2-34a4df448ad4a8020757f09f15b8f6ce/Laboratoire13_2/ModelsTests/MontreTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Tests
+{
+
+    [TestClass()]
+    public class MontreTests
+    {
+        public const int Temp_compris_MIN = 0;
+        public const int Heur_Compris_MAX = 23;
+        public const int MIN_Compris_MAX = 59;
+        public const int SECONDE_Compris_MAX = 59;
+
+
+
+        [TestMethod()]
+        public void MontreTest()
+        {
+            Montre m = new Montre(11,33,33);
+            Assert.AreEqual("11:33:33",m.ObtenirTempsCourant()); 
+        }
+
+        [TestMethod()]
+        public void Montre_Aucune_Donnees_Test()
+        {
+            Montre m = new Montre();
+            Assert.AreEqual("00:00:00", m.ObtenirTempsCourant());
+        }
+
+
+        [TestMethod()]
+        public void AvancerUneSecondeTest()
+        {
+            Montre m = new Montre(11, 33, 33);
+            m.AvancerUneSeconde();
+            Assert.AreEqual("11:33:34", m.ObtenirTempsCourant());
+            
+        }
+
+        [TestMethod()]
+        public void ObtenirTempsCourantTest()
+        {
+            Montre m = new Montre(11, 33, 33);
+            
+
+            Assert.AreEqual("11:33:33", m.ObtenirTempsCourant());
+        }
+    }
+}
